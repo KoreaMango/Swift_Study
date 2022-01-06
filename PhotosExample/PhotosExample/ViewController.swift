d30883ffc79ce33995f04c375b1f555894ea70ee
KoreaMango <mgo8434@naver.com>
2022-01-06 16:51:26 +0900

    get_Photos

---
+++ PhotosExample/PhotosExample/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  PhotosExample
+//
+//  Created by 강민규 on 2022/01/06.
+//
+
+import UIKit
+import Photos
+
+class ViewController: UIViewController ,UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var fetchResult: PHFetchResult<PHAsset>!
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    let cellIdentifier: String = "cell"
+    
+    func requestCollection(){
+        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        
+        guard let cameraRollCollection = cameraRoll.firstObject else{
+            return
+        }
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAuthorizationStatus {
+        case .authorized:
+            print("접근 허가됨")
+            self.requestCollection()
+            self.tableView.reloadData()
+        case .denied:
+            print("접근불허")
+        case .notDetermined:
+            print("아직 응답하지 않음")
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                switch status{
+                case .authorized:
+                    print("사용자가 허용함")
+                    self.requestCollection()
+                    OperationQueue.main.addOperation {
+                        self.tableView.reloadData()
+                    }
+                   
+                case .denied:
+                    print("사용자가 불허함")
+                default: break
+                }
+            })
+        case .restricted:
+            print("접근 제한")
+        default : break
+        }
+        
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fetchResult?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        let asset : PHAsset = fetchResult.object(at: indexPath.row)
+        
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: {image, _ in cell.imageView?.image = image})
+        return cell
+    }
+}
+
