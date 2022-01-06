b8adf41b9a76d4b62f43044041253f3b4001570d
KoreaMango <mgo8434@naver.com>
2022-01-06 17:34:55 +0900

    del_Photo

--- PhotosExample/PhotosExample/ViewController.swift
+++ PhotosExample/PhotosExample/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController ,UITableViewDataSource{
+class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelegate, PHPhotoLibraryChangeObserver{
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -59,11 +59,28 @@ class ViewController: UIViewController ,UITableViewDataSource{
             })
         case .restricted:
             print("접근 제한")
+            
+        
         default : break
         }
-        
+        PHPhotoLibrary.shared().register(self)
         
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let asset: PHAsset = self.fetchResult[indexPath.row]
+            
+            PHPhotoLibrary.shared().performChanges ({
+                PHAssetChangeRequest.deleteAssets([asset] as NSArray)
+            }, completionHandler: nil)
+        }
+    }
+    
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.fetchResult?.count ?? 0
@@ -76,5 +93,18 @@ class ViewController: UIViewController ,UITableViewDataSource{
         imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: {image, _ in cell.imageView?.image = image})
         return cell
     }
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+            else {return}
+        
+        fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
+        }
+    }
+    
+    
 }
 
