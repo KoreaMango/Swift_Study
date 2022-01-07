46a8d9240e2808da084e33b41991a889c44c3102
KoreaMango <mgo8434@naver.com>
2022-01-07 18:36:06 +0900

    MakeCollectionView

---
+++ FriendsCollection/FriendsCollection/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  FriendsCollection
+//
+//  Created by 강민규 on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    var numberOfCell: Int = 10
+    let cellIdentifier: String = "cell"
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.numberOfCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
