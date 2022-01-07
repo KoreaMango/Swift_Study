1ac5522ad36c0ea0b369164feafc0f420e700fdb
KoreaMango <mgo8434@naver.com>
2022-01-07 21:18:09 +0900

    CustomCollectionView

---
+++ FriendsCollection/FriendsCollection/FriendCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendCollectionViewCell.swift
+//  FriendsCollection
+//
+//  Created by 강민규 on 2022/01/07.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var adressLabel: UILabel!
+    
+}
