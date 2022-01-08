ad7e0d5ee733aae6c82342be800a8f1db8a06848
KoreaMango <mgo8434@naver.com>
2022-01-08 13:39:49 +0900

    TabBar

---
+++ TabBarInterface/TabBarInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarInterface
+//
+//  Created by 강민규 on 2022/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
