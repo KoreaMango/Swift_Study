4c06a09c1cd9e837b8ccae2051d00a1c3c55b826
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-02-16 15:18:01 +0900

    TableView Study

---
+++ CodersHigh/EmojiTable/EmojiTable/Emoji.swift
@@ -0,0 +1,23 @@
+//
+//  Emoji.swift
+//  EmojiTable
+//
+//  Created by 강민규 on 2022/02/16.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+    
+}
---
+++ CodersHigh/EmojiTable/EmojiTable/EmojiTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiTable
+//
+//  Created by 강민규 on 2022/02/16.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(with emoji: Emoji) {
+        symbolLabel.text = emoji.symbol
+        descriptionLabel.text = emoji.description
+        nameLabel.text = emoji.name
+    }
+    
+    
+
+}
