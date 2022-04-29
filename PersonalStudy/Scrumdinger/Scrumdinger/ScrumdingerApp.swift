d05c9f7c41511b35194e711060005c65d88f9f6c
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-04-30 05:15:02 +0900

    Scrumdinger init

---
+++ PersonalStudy/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 강민규 on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
