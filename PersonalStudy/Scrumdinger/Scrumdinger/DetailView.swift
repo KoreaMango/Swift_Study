eedcfe98b050ef1b0b6990781ff1b666766efb84
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-09 01:24:41 +0900

    SwiftUI chapter 2 Section 2

---
+++ PersonalStudy/Scrumdinger/Scrumdinger/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViedw.swift
+//  Scrumdinger
+//
+//  Created by 강민규 on 2022/05/09.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailViedw_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
